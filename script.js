d031a65a95061a71f45e44f4f4406e6dd65686bd
mariaelima73 <maria.e.lima73@aluno.senai.br>
2024-04-22 10:49:28 -0300

    Dia 4

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
 //INÍCIO JAVASCRIPT
 
-var category = document.getElementById('catPaes')
+var catPaes = document.getElementById('catPaes')
 
-        function clicar(){
-            navegarParaUrl(paes.html)
-            
-        }
+    function clicarPaes(){
+        location.href = 'http://127.0.0.1:5500/paes.html'
+    }
+
+var catDoces = document.getElementById('catDoc')
+
+    function clicarDoces(){
+        location.href = 'http://127.0.0.1:5500/doces.html'
+    }
+
+var catSalgados = document.getElementById('catSalg')
+
+    function clicarSalgados(){
+        location.href = 'http://127.0.0.1:5500/salgados.html'
+    }
+
+var catBebidas = document.getElementById('catBeb')
+
+    function clicarBebidas(){
+        location.href = 'http://127.0.0.1:5500/bebidas.html'
+    }
+
+/*var catBebidas = document.getElementById('car')
+
+    function clicarCarrinho(){
+        location.href = 'http://127.0.0.1:5500/carrinho.html'
+    }*/
 
 
 
